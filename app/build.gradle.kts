ea355d26e71882694386a1a98b26f56fb284aed6
Florian Friesenegger <dev@friesenegger.xyz>
2025-04-18 19:18:40 +0200

    Increase version
    Signed-off-by: frieseneggerf <dev@friesenegger.xyz>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "de.floriii.link2lib"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0-beta"
+        versionCode = 4
+        versionName = "1.0-beta2"
         setProperty("archivesBaseName", "link2lib-v$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
